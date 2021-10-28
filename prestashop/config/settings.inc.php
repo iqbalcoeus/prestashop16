8a51103f2b61a19b2eb85a3f62ea4a456486f269
Your Name <you@example.com>
2021-10-28 13:02:33 +0100

    first commit

---
+++ prestashop/config/settings.inc.php
@@ -0,0 +1,16 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'testing_qa');
+define('_DB_USER_', 'testing_qa');
+define('_DB_PASSWD_', 'coeus123#');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 'GyC5E5Mpw5fK3C6JsFTOfnjwkEewfzErM6KvQ9CRzKh6xqYhHrKm6gg6');
+define('_COOKIE_IV_', 'JuCNJFQu');
+define('_PS_CREATION_DATE_', '2021-10-28');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.1.24');
+define('_RIJNDAEL_KEY_', 'HXtow5hEiRH7uYrGdi4bksw7QQLm0Kgi');
+define('_RIJNDAEL_IV_', 'a2cTmsHn0WRZMzuob0PpnQ==');
